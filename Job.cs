099ed08efa17a23510e73bd1af96b8575f6dacca
İbrahim Bali <balii@pm.me>
2021-01-15 17:16:07 +0300

    initial commit

---
+++ Job.cs
@@ -0,0 +1,14 @@
+﻿using Quartz;
+using System.Threading.Tasks;
+
+namespace RunBinary
+{
+    public class Job : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var batchConfig = Utility.Configuration;
+            await batchConfig.File.RunProcessAsync(batchConfig.Arguments);
+        }
+    }
+}
---
+++ ProcessExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RunBinary
+{
+    internal static class ProcessExtensions
+    {
+        internal static async Task RunProcessAsync(this string filename, string arguments)
+        {
+            //* Create your Process
+            using var process = new Process();
+            process.StartInfo.FileName = filename;
+            process.StartInfo.Arguments = arguments;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.EnableRaisingEvents = true;
+            process.Exited += process.ExitHandler;
+            process.Start();
+            await process.WaitForExitAsync();
+        }
+
+        private static void ExitHandler(this Process process, object _, EventArgs __)
+        {
+            if (!process.HasExited) return;
+            if (process.ExitCode == 0)
+            {
+                var message = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(message);
+            }
+            else
+            {
+                var errorMessage = $"ERROR running:\n{process.StartInfo.FileName} {process.StartInfo.Arguments}\n{ process.StandardError.ReadToEnd()}";
+                //Console.WriteLine(errorMessage);
+                throw new Exception(errorMessage);
+            }
+        }
+    }
+}
